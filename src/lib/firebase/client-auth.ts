79f9e45745450734fdf600f920577005b18d9c7f
Edwin Aguiar <a.cresidenciaselvallecobranzas@gmail.com>
2025-11-25 09:34:46 +0000

    Corrige el archivo `src/lib/firebase/client-auth.ts` para que el import

--- src/lib/firebase/client-auth.ts
+++ src/lib/firebase/client-auth.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./config";
+import { firebaseConfig } from "@/firebase/config";
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
